b97457c6ff9e1e6f9726155ced1c1e85b7be4893
seyeong <seyoung7555@naver.com>
2021-08-04 15:18:35 +0900

    Spring security 프로젝트 시작

---
+++ src/main/java/controller/IndexController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller // View 를 리턴하겠다.
+public class IndexController {
+
+    @GetMapping({"", "/"})
+    public String index() {
+        return "index";
+    }
+}
---
+++ src/test/java/com/seyeong/security1/Security1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seyeong.security1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Security1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
